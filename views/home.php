113131e3885c265dea3b0d32a087bbd70f126791
JLob Lap <joselobatof@gmail.com>
2020-10-03 00:33:28 -0500

    vistas home y layout

---
+++ views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1>Página Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, fugit! Soluta ullam quidem qui officiis iure eum distinctio porro nemo inventore, repudiandae tempore? Minima, quam consectetur illo eveniet sint aut.</p>
+</div>
